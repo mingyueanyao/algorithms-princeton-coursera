4dfc012c96926d58466747fcf2b3449e8d12922a
archeroc <1981167302@qq.com>
2019-02-01 10:29:39 +0800

    First commit on branch part1-pa5

---
+++ Codes of Programming Assignments/part1/pa5-kdtree/PointSET.java
@@ -0,0 +1,100 @@
+import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.RectHV;
+import edu.princeton.cs.algs4.Stack;
+import edu.princeton.cs.algs4.SET;
+
+import java.util.Iterator;
+
+public class PointSET {
+    private SET<Point2D> points;
+
+    // construct an empty set of points 
+    public PointSET() {
+        points = new SET<Point2D>();
+    }     
+    
+    // is the set empty? 
+    public boolean isEmpty() {
+        return points.isEmpty();
+    }            
+    
+    // number of points in the set 
+    public int size() {
+        return points.size();
+    }   
+    
+    // add the point to the set (if it is not already in the set)
+    public void insert(Point2D p) {
+        if (p == null) {
+            throw new IllegalArgumentException("argument to insert() is null\n");
+        }
+
+        if (!points.contains(p)) {
+            points.add(p);
+        }   
+    }       
+    
+    // does the set contain point p? 
+    public boolean contains(Point2D p) {
+        if (p == null) {
+            throw new IllegalArgumentException("argument to contains() is null\n");
+        }
+
+        return points.contains(p);
+    }   
+    
+    // draw all points to standard draw 
+    public void draw() { 
+        for (Iterator<Point2D> iter = points.iterator(); iter.hasNext();) {
+            Point2D p = iter.next();
+            p.draw();
+        }
+    } 
+    
+     // all points that are inside the rectangle (or on the boundary) 
+    public Iterable<Point2D> range(RectHV rect) { 
+        if (rect == null) {
+            throw new IllegalArgumentException("argument to range() is null\n");
+        }
+
+        double xmin = rect.xmin();
+        double ymin = rect.ymin();
+        double xmax = rect.xmax();
+        double ymax = rect.ymax();
+        Stack<Point2D> pointsInRect = new Stack<Point2D>();
+        for (Iterator<Point2D> iter = points.iterator(); iter.hasNext();) {
+            Point2D p = iter.next();
+            double x = p.x();
+            double y = p.y();
+            if (x >= xmin && x <= xmax && y >= ymin && y <= ymax) {
+                pointsInRect.push(p);
+            }
+        }
+        return pointsInRect;
+    }  
+    
+    // a nearest neighbor in the set to point p; null if the set is empty 
+    public Point2D nearest(Point2D p) {
+        if (p == null) {
+            throw new IllegalArgumentException("argument to nearest() is null\n");
+        }
+
+        if (points.isEmpty()) {
+            return null;
+        }
+
+        double dist = 0xffff;
+        Point2D nearestP = null;
+        for (Iterator<Point2D> iter = points.iterator(); iter.hasNext();) {
+            Point2D tmp = iter.next();
+            if (tmp.distanceSquaredTo(p) < dist) {
+                dist = tmp.distanceSquaredTo(p);
+                nearestP = tmp;
+            }
+        }
+        return nearestP;
+    }             
+ 
+    // unit testing of the methods (optional) 
+    public static void main(String[] args)  { }                
+ }
---
+++ Notes of Courses/part1/pa5-kd-trees.md
@@ -0,0 +1,17 @@
+# 编程作业五
+
+作业链接：[Kd-Trees](http://coursera.cs.princeton.edu/algs4/assignments/kdtree.html) & [Checklist](http://coursera.cs.princeton.edu/algs4/checklists/kdtree.html)
+
+我的代码：[PointSET.java](https://github.com/mingyueanyao/algorithms-princeton-coursera/blob/master/Codes%20of%20Programming%20Assignments/part1/pa5-kdtree/PointSET.java) & [KdTree.java](https://github.com/mingyueanyao/algorithms-princeton-coursera/blob/master/Codes%20of%20Programming%20Assignments/part1/pa5-kdtree/KdTree.java)
+
+## 问题简介
+
+## 任务摘要
+
+详细参见：[specification](http://coursera.cs.princeton.edu/algs4/assignments/kdtree.html)。
+
+## 问题分析
+
+## 测试结果
+
+![part1-pa5-result](https://img2018.cnblogs.com/blog/886021/201902/886021-20190201102706920-903459761.png)
